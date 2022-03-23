2bbf6fa511432e236dfce5ca9a032ae1d05fcd81
Riley Johnston <RileyJohnston@cmail.carleton.ca>
2022-03-23 16:52:45 -0400

    Jon's Updates

--- test.py
+++ test.py
@@ -1,20 +1,17 @@
 
 # Used to handle the images obtained from the dataset. 
 import nntplib
-from PIL import Image, ImageStat
+from PIL import Image
 
 # Directory functions
 import os 
 # Mathematical operations
-import numpy as np
-from torch import tensor 
+import numpy as np 
 
 # import the building blocks for the neural nets
 from torch.nn import Linear, ReLU, MSELoss, Sequential, Conv2d, MaxPool2d, Module, Dropout
 # import the optimizer function
 from torch.optim import SGD
-# function to generate the train and validation split 
-from sklearn.model_selection import train_test_split
 # used for image processing
 import torchvision.transforms
 
@@ -23,7 +20,6 @@ import torchvision.transforms
 
 # ------ Retrieve all the Training Images -------#
 train_img = []
-train_lbl = []
 
 # Path containing the training images 
 path = 'data\GTSRB\Final_Training\Images'
@@ -43,28 +39,21 @@ for dir in set_dir:
         # if it is a jpg file, then proceed
         if file.name.endswith(".jpg"):
             img = Image.open(dir_path + "\\" + file.name)
-                     
-            # Configure the conversion to tensor
+            train_img.append(img)            
+            
+            
             transform = torchvision.transforms.Compose([
-                # convert the image to a tensor of range 0->1
                 torchvision.transforms.ToTensor(), 
+                torchvision.transforms.Normalize(
+                    mean = 
+                )
             ])
-            # apply the transform
-            tensor_img = transform(img)
 
-            # Add the new tensor to the list
-            train_img.append(tensor_img)
-            # label is stored in directory index   
-            train_lbl.append(dir_name)
-            # Close the file
             img.close()
 
-# Convert to a numpy array
-train_img = np.array(train_img) # images used for the training process
 
-# Create a train & validation split w/ 0.1 sent to validation
-train_img, val_img, train_lbl, val_lbl = train_test_split(train_img, train_lbl, test_size=0.1)
 
+print(len(train_img))
 
 
 
