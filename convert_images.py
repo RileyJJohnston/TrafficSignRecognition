f9c41a4f9651084db209b78b277f5e45afa41675
Riley Johnston <RileyJohnston@cmail.carleton.ca>
2022-03-23 15:30:32 -0400

    Initial commit.  Uploaded image files and scripts.

---
+++ convert_images.py
@@ -0,0 +1,42 @@
+# This code is used to convert all the images from ppm file format to jpg
+from PIL import Image
+import os 
+import string
+
+
+#Root folder path
+path = 'data\GTSRB\Final_Training\Images'
+
+# Obtain the set of directories
+set_dir = os.scandir(path)
+
+# For each image directory
+for dir in set_dir: 
+    #obtain the name of the directory
+    dir_name = dir.name
+
+    dir_path = path + '\\' + dir_name
+
+    # Obtain all the images in the directory
+    imgs = os.scandir(dir_path)
+    # for each img
+    for img in imgs: 
+        # if it is a ppm file, then proceed
+        if img.name.endswith(".ppm"):
+            # obtain the image
+            im = Image.open(dir_path + '\\' + img.name)
+            # change the file extension to jpg
+            name = img.name.replace(".ppm", ".jpg")
+            # Save the new jpg file
+            im.save(dir_path + '\\' + name)           
+            
+    
+
+
+
+
+set_dir.close()
+
+
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+
+# Used to handle the images obtained from the dataset. 
+from PIL import Image
+# Directory functions
+import os 
+
+
+
+
+
+
+
+
+
