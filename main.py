efb1901c61ddb3b77460d07ba1fb16e7eeb3d991
Riley Johnston <RileyJohnston@cmail.carleton.ca>
2022-03-24 13:58:16 -0400

    Added training process.  Creates numpy array from images.  NOT WORKING.

--- main.py
+++ main.py
@@ -1,98 +1,191 @@
 
-# Used to handle the images obtained from the dataset. 
+# Used to handle the images obtained from the dataset.
 import nntplib
 from PIL import Image, ImageStat
 
 # Directory functions
-import os 
+import os
 # Mathematical operations
 import numpy as np
 import torch
 
 # import the building blocks for the neural nets
-from torch.nn import Linear, ReLU, MSELoss, Sequential, Conv2d, MaxPool2d, Module, Dropout
+from torch.nn import Linear, ReLU, CrossEntropyLoss, MSELoss, Sequential, Conv2d, MaxPool2d, Module, Dropout
 # import the optimizer function
 from torch.optim import SGD
-# function to generate the train and validation split 
+# function to generate the train and validation split
 from sklearn.model_selection import train_test_split
 # used for image processing
 import torchvision.transforms
+# import optimizer and loss function
+from torch.optim import Adam, SGD
+# import wrapper for tensors
+from torch.autograd import Variable
+# import time to measure time taken by aspects of the program
+import time
 
+from skimage.io import imread
 
+start = time.time()
 
 # ------ Retrieve all the Training Images -------#
 train_img = []
 train_lbl = []
 
-# Path containing the training images 
+# Path containing the training images
 path = 'data\GTSRB\Final_Training\Images'
 # Obtain the set of directories
 set_dir = os.scandir(path)
 
 # For each image directory
-for dir in set_dir: 
+for dir in set_dir:
     #obtain the name of the directory
     dir_name = dir.name
     dir_path = path + '\\' + dir_name
 
     # Obtain all the images in the directory
     files = os.scandir(dir_path)
+
+    train_img = []
+    train_lbl = []
     # for each img
-    for file in files: 
+    for file in files:
         # if it is a jpg file, then proceed
         if file.name.endswith(".jpg"):
-            img = Image.open(dir_path + "\\" + file.name)
-                     
+            filePath = dir_path + "\\" + file.name
+            img = imread(filePath, as_gray=True)
+            # normalizing the pixel values
+            img /= 255.0
+            # converting the type of pixel to float 32
+            img = img.astype('float32')
             # Configure the conversion to tensor
-            transform = torchvision.transforms.Compose([
-                # convert the image to a tensor of range 0->1
-                torchvision.transforms.ToTensor(), 
-            ])
+           # transform = torchvision.transforms.Compose([
+            # convert the image to a tensor of range 0->1
+           #     torchvision.transforms.ToTensor(),
+            #])
             # apply the transform
-            tensor_img = transform(img)
+            #tensor_img = transform(img)
 
             # Add the new tensor to the list
-            train_img.append(tensor_img)
-            # label is stored in directory index   
+            train_img.append(img)
+            # label is stored in directory index
             train_lbl.append(dir_name)
             # Close the file
             img.close()
 
+end = time.time()
+print(end-start)
+
 # Convert to a numpy array
-train_img = np.array(train_img) # images used for the training process
+#train_img = np.array(train_img) # images used for the training process
+train_img = np.array(train_img)
+train_lbl = np.array(train_lbl)
 
 # Create a train & validation split w/ 0.1 sent to validation
-train_img, val_img, train_lbl, val_lbl = train_test_split(train_img, train_lbl, test_size=0.1)
+train_img, val_img, train_lbl, val_lbl = train_test_split(
+    train_img, train_lbl, test_size=0.1)
+print(type(train_img))
 
-# convert from numpy array to a tensor
 train_img = torch.from_numpy(train_img)
-val_img = torch.from_numpy(val_img)
+train_lbl = torch.from_numpy(train_lbl)
+
+#train_img = torch.from_numpy(train_img)
+#train_img = torch.from_numpy(train_lbl)
+print(type(train_img))
+# convert from numpy array to a tensor
+#train_img = torch.from_numpy(train_img)
+#val_img = torch.from_numpy(val_img)
 
 # Neural Net architecture
-class NN(Module): 
-    def __init__(self): 
+
+
+class NN(Module):
+    def __init__(self):
         super(NN, self).__init__()
 
         # Defining the sequential layers for the NN
         self.cnn_layers = Sequential(
             # 2D Convolutional layer
-            Conv2d(1,4, kernel_size=3,stride=1,padding=1),
+            Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
             ReLU(inplace=True),
-            MaxPool2d(kernel_size=2, stride=2), 
+            MaxPool2d(kernel_size=2, stride=2),
             # 2nd 2D Convolutonal Layer
-            Conv2d(4,4,kernel_size=3, stride=1,padding=1), 
+            Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
             ReLU(inplace=True),
-            MaxPool2d(kernel_size=2,stride = 2),
+            MaxPool2d(kernel_size=2, stride=2),
         )
 
         self.linear_layers = Sequential(
             Linear(4*7*7, 42)
         )
 
-
     # Forward pass
-    def forward(self, x): 
+    def forward(self, x):
+        print("in forward")
         x = self.cnn_layers(x)
         x = x.view(x.size(0), -1)
         x = self.linear_layers(x)
+        print("complete")
+        print(x)
         return x
+
+
+def train(epoch):
+    model.train()
+    tr_loss = 0
+    # getting the training set
+    x_train, y_train = Variable(train_img), Variable(train_lbl)
+    # getting the validation set
+    x_val, y_val = Variable(val_img), Variable(val_lbl)
+    # converting the data into GPU format
+    if torch.cuda.is_available():
+        x_train = x_train.cuda()
+        y_train = y_train.cuda()
+        x_val = x_val.cuda()
+        y_val = y_val.cuda()
+
+    # clearing the Gradients of the model parameters
+    optimizer.zero_grad()
+
+    # prediction for training and validation set
+    output_train = model(x_train)
+    output_val = model(x_val)
+
+    # computing the training and validation loss
+    loss_train = criterion(output_train, y_train)
+    loss_val = criterion(output_val, y_val)
+    train_losses.append(loss_train)
+    val_losses.append(loss_val)
+
+    # computing the updated weights of all the model parameters
+    loss_train.backward()
+    optimizer.step()
+    tr_loss = loss_train.item()
+    if epoch % 2 == 0:
+        # printing the validation loss
+        print('Epoch : ', epoch+1, '\t', 'loss :', loss_val)
+
+
+# construct the model defined above
+model = NN()
+# define the optimizer
+optimizer = Adam(model.parameters(), lr=0.07)
+# define the loss function
+criterion = CrossEntropyLoss()
+# check if a GPU is available
+if torch.cuda.is_available():
+    model = model.cuda()
+    criterion = criterion.cuda()
+
+print(model)
+
+
+# defining the number of epochs
+n_epochs = 25
+# empty list to store training losses
+train_losses = []
+# empty list to store validation losses
+val_losses = []
+# training the model
+for epoch in range(n_epochs):
+    train(epoch)
